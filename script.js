b4d9f634daec3e9d74a9f64293c42b3727113196
amarispurice <amaris.purice@emmanuel.ro>
2024-04-03 16:23:05 +0300

    am adaugat a sasea caseta

--- script.js
+++ script.js
@@ -43,4 +43,21 @@ function Caseta(x,y) {
     textSize(32); 
     text("5", x+480, y+40);
 
+    fill(" #2196F3")
+    rect(x+550, y, 100, 60 );
+    fill("#000000");
+    textSize(32); 
+    text("6", x+590, y+40);
+
+
+
+
+
+
+
+
+
+
+
+    
 }

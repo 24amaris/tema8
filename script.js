70d4688ddf46e0ea211ed3a1648ae166118fc6fe
amarispurice <amaris.purice@emmanuel.ro>
2024-04-03 16:17:44 +0300

    am adaugat a doua caseta

--- script.js
+++ script.js
@@ -13,9 +13,21 @@ function draw() {
 }
 
 function Caseta(x,y) {
-  fill("#F44336");
-  rect(x, y, 100, 60 );
-  fill("#000000");
-  textSize(32); 
-  text("1", x+40, y+40); 
+    fill("#F44336");
+    rect(x, y, 100, 60 );
+    fill("#000000");
+    textSize(32); 
+    text("1", x+40, y+40); 
+
+    fill(" #E91E63")
+    rect(x+110, y, 100, 60 );
+    fill("#000000");
+    textSize(32); 
+    text("2", x+150, y+40);
+
+
+
+
+
+
 }

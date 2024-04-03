2cffadcc23956bd9fc09b38c663c2196ebf1e80c
amarispurice <amaris.purice@emmanuel.ro>
2024-04-03 16:32:53 +0300

    am finalizat problema

--- script.js
+++ script.js
@@ -56,96 +56,132 @@ function Caseta(x,y) {
     text("7", x+700, y+40);
 
 //a doua linie
-fill("#00BCD4");
-rect(x, y+100, 100, 60 );
-fill("#000000");
-textSize(32); 
-text("8", x+40, y+140); 
+    fill("#00BCD4");
+    rect(x, y+100, 100, 60 );
+    fill("#000000");
+    textSize(32); 
+    text("8", x+40, y+140); 
 
-fill(" #009688");
-rect(x+110, y+100, 100, 60 );
-fill("#000000");
-textSize(32); 
-text("9", x+150, y+140); 
+    fill(" #009688");
+    rect(x+110, y+100, 100, 60 );
+    fill("#000000");
+    textSize(32); 
+    text("9", x+150, y+140); 
 
-fill(" #4CAF50");
-rect(x+220, y+100, 100, 60 );
-fill("#000000");
-textSize(32); 
-text("10", x+255, y+140); 
+    fill(" #4CAF50");
+    rect(x+220, y+100, 100, 60 );
+    fill("#000000");
+    textSize(32); 
+    text("10", x+255, y+140); 
 
-fill(" #8BC34A");
-rect(x+330, y+100, 100, 60 );
-fill("#000000");
-textSize(32); 
-text("11", x+360, y+140); 
+    fill(" #8BC34A");
+    rect(x+330, y+100, 100, 60 );
+    fill("#000000");
+    textSize(32); 
+    text("11", x+360, y+140); 
 
-fill(" #CDDC39");
-rect(x+440, y+100, 100, 60 );
-fill("#000000");
-textSize(32); 
-text("12", x+470, y+140); 
+    fill(" #CDDC39");
+    rect(x+440, y+100, 100, 60 );
+    fill("#000000");
+    textSize(32); 
+    text("12", x+470, y+140); 
 
-fill(" #FFEB3B");
-rect(x+550, y+100, 100, 60 );
-fill("#000000");
-textSize(32); 
-text("13", x+580, y+140); 
+    fill(" #FFEB3B");
+    rect(x+550, y+100, 100, 60 );
+    fill("#000000");
+    textSize(32); 
+    text("13", x+580, y+140); 
 
-fill(" #FFC107");
-rect(x+660, y+100, 100, 60 );
-fill("#000000");
-textSize(32); 
-text("14", x+690, y+140);
+    fill(" #FFC107");
+    rect(x+660, y+100, 100, 60 );
+    fill("#000000");
+    textSize(32); 
+    text("14", x+690, y+140);
 
 //linia3
-fill("#FF9800");
-rect(x, y+200, 100, 60 );
-fill("#000000");
-textSize(32); 
-text("15", x+30, y+240);
+    fill("#FF9800");
+    rect(x, y+200, 100, 60 );
+    fill("#000000");
+    textSize(32); 
+    text("15", x+30, y+240);
 
-fill(" #FF5722");
-rect(x+110, y+200, 100, 60 );
-fill("#000000");
-textSize(32); 
-text("16", x+140, y+240);
+    fill(" #FF5722");
+    rect(x+110, y+200, 100, 60 );
+    fill("#000000");
+    textSize(32); 
+    text("16", x+140, y+240);
 
-fill(" #795548");
-rect(x+220, y+200, 100, 60 );
-fill("#000000");
-textSize(32); 
-text("17", x+250, y+240);
+    fill(" #795548");
+    rect(x+220, y+200, 100, 60 );
+    fill("#000000");
+    textSize(32); 
+    text("17", x+250, y+240);
 
-fill(" #9E9E9E");
-rect(x+330, y+200, 100, 60 );
-fill("#000000");
-textSize(32); 
-text("18", x+360, y+240);
+    fill(" #9E9E9E");
+    rect(x+330, y+200, 100, 60 );
+    fill("#000000");
+    textSize(32); 
+    text("18", x+360, y+240);
 
-fill(" #607D8B");
-rect(x+440, y+200, 100, 60 );
-fill("#000000");
-textSize(32); 
-text("19", x+470, y+240);
+    fill(" #607D8B");
+    rect(x+440, y+200, 100, 60 );
+    fill("#000000");
+    textSize(32); 
+    text("19", x+470, y+240);
 
-fill(" #F44336");
-rect(x+550, y+200, 100, 60 );
-fill("#000000");
-textSize(32); 
-text("20", x+580, y+240);
+    fill(" #F44336");
+    rect(x+550, y+200, 100, 60 );
+    fill("#000000");
+    textSize(32); 
+    text("20", x+580, y+240);
 
-fill(" #E91E63");
-rect(x+660, y+200, 100, 60 );
-fill("#000000");
-textSize(32); 
-text("21", x+690, y+240);
+    fill(" #E91E63");
+    rect(x+660, y+200, 100, 60 );
+    fill("#000000");
+    textSize(32); 
+    text("21", x+690, y+240);
 
+//linia4
+    fill("#9C27B0");
+    rect(x, y+300, 100, 60 );
+    fill("#000000");
+    textSize(32); 
+    text("22", x+30, y+340);
 
+    fill(" #673AB7")
+    rect(x+110, y+300, 100, 60 );
+    fill("#000000");
+    textSize(32); 
+    text("23", x+140, y+340);
 
+    fill(" #3F51B5");
+    rect(x+220, y+300, 100, 60 );
+    fill("#000000");
+    textSize(32); 
+    text("24", x+250, y+340);
 
+    fill(" #2196F3");
+    rect(x+330, y+300, 100, 60 );
+    fill("#000000");
+    textSize(32); 
+    text("25", x+360, y+340);
 
+    fill(" #03A9F4");
+    rect(x+440, y+300, 100, 60 );
+    fill("#000000");
+    textSize(32); 
+    text("26", x+470, y+340);
 
+    fill(" #00BCD4");
+    rect(x+550, y+300, 100, 60 );
+    fill("#000000");
+    textSize(32); 
+    text("27", x+580, y+340);
 
+    fill(" #009688");
+    rect(x+660, y+300, 100, 60 );
+    fill("#000000");
+    textSize(32); 
+    text("28", x+690, y+340);
 
 }
